16aea901d969eaefd8e5d56b268e0c08892fa339
ScottHahn1 <scotthahn2210@gmail.com>
2024-12-01 10:12:20 +0200

    Added custom hooks useFetch and useFetchInfinite for data fetching with React Query.

---
+++ app/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+// 'use client';
+import { QueryFunctionContext, useInfiniteQuery, useQuery } from "@tanstack/react-query";
+
+const useFetch = <T>(url: string, queryKey: string[], enabled: boolean) => {
+    const fetchData = async () => {
+        const res = await fetch(url)
+        return res.json();
+    };
+
+    const { data } = useQuery<T>({
+        queryKey: queryKey,
+        queryFn: fetchData,
+        enabled: enabled
+    });
+
+    return { data };
+}
+
+const useFetchInfinite = <T extends { nextPageToken: string }>(url: string, queryKey: string[]) => {
+    const fetchData = async ({ pageParam = '' }: QueryFunctionContext) => {
+        const fullUrl = pageParam ? `${url}&pageToken=${pageParam}` : url;
+        const res = await fetch(fullUrl);
+        return res.json();
+    };
+
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess } = useInfiniteQuery<T>({
+        queryKey: queryKey,
+        queryFn: fetchData,
+        initialPageParam: '',
+        getNextPageParam: (lastPage: T) => lastPage.nextPageToken,
+    });
+
+    return { data, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess };
+}
+
+export { useFetch, useFetchInfinite };
