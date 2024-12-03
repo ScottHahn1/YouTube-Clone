dd7fded77b8a075de1192c6b19f9b4056e652e43
ScottHahn1 <scotthahn2210@gmail.com>
2024-12-03 20:44:20 +0200

    Feature: Created Popular component for displaying popular videos on the homepage with infinite scrolling and channel data integration

---
+++ app/components/home/popular.tsx
@@ -0,0 +1,82 @@
+'use client';
+import { useFetchInfinite } from "@/app/hooks/useFetch";
+import Card from "./card";
+import useChannels from "../../hooks/useChannels";
+import useInfiniteScroll from "@/app/hooks/useInfiniteScroll";
+
+export interface Videos {
+    items: {
+        id: string;
+        contentDetails: {
+            duration: string;
+        }
+        snippet: {
+            channelId: string;
+            channelTitle: string;
+            publishedAt: string;
+            thumbnails: {
+                high: {
+                    url: string;
+                }
+            }
+            title: string;
+        }
+        statistics: {
+            viewCount: number;
+        }
+    }[];
+    nextPageToken: string;
+}
+
+export interface ChannelsData {
+    items: {
+        id: string;
+        snippet: {
+            thumbnails: {
+                default: {
+                    url: string;
+                }
+            }
+            title: string;
+        }
+    }[]
+    nextPageToken: string;
+}
+
+const Popular = () => {
+    const videosQueryParams = new URLSearchParams({
+        'part': 'snippet',
+        'chart': 'mostPopular',
+        'maxResults': '30'
+    }).toString();
+
+    const { data: videos, fetchNextPage, hasNextPage, isFetchingNextPage } = useFetchInfinite<Videos>(`http://localhost:3000/api/videos?${videosQueryParams}`, ['popular', videosQueryParams]);
+
+    const channels = useChannels(videos?.pages.length ? videos?.pages[videos.pages.length - 1].items.map(page => page.snippet.channelId) : []);
+
+    const lastItemRef = useInfiniteScroll(fetchNextPage, hasNextPage, isFetchingNextPage);
+
+    return (
+        <div className='w-80% bg-red-500 flex flex-wrap float-right'>
+            {
+                videos?.pages.map(page => page.items.map((video, index) => {
+                    
+                    return (
+                        <>  
+                            <Card 
+                                key={video.id}
+                                channelImage={channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.thumbnails.default.url as string || ''}
+                                channelTitle= {channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.title as string || ''} 
+                                title={video.snippet.title} 
+                                thumbnail={video.snippet.thumbnails.high.url} 
+                            />
+                            { index === page.items.length - 1 && <div ref={lastItemRef} /> }
+                        </>
+                    )
+                }))
+            }
+        </div>
+    )
+}
+
+export default Popular;
