7605da8b5db07e2cbd4d7fb17cb1191c921bcc46
ScottHahn1 <scotthahn2210@gmail.com>
2024-12-03 20:42:28 +0200

    Feature: Added useChannels hook for fetching and managing YouTube channel data

---
+++ app/hooks/useChannels.ts
@@ -0,0 +1,38 @@
+import { ChannelsData } from "../components/home/popular";
+import { useFetch } from "@/app/hooks/useFetch";
+import { useEffect, useState } from "react";
+
+interface Channels {
+    id: string;
+    snippet: {
+        thumbnails: {
+            default: {
+                url: string;
+            }
+        }
+        title: string;
+    }
+}
+
+const useChannels = (channelIds: string[]) => {
+    const [channels, setChannels] = useState<Channels[]>([]);
+
+    const channelsQueryParams = new URLSearchParams({
+        'id': channelIds?.join(',') as string,
+        'part': 'snippet',
+        'maxResults': '30'
+    }).toString();
+
+    const apiUrl = `http://localhost:3000/api/channels?${channelsQueryParams}`;
+    const { data: channelsData } = useFetch<ChannelsData>(apiUrl, ['channels', channelsQueryParams], !!channelIds.length);
+
+    useEffect(() => {
+        if (channelsData) {
+            setChannels(prev => [...prev, ...channelsData.items]);
+        }
+    }, [channelsData])
+
+    return channels;
+}
+
+export default useChannels;
