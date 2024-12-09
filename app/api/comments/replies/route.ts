bf51b57b8ffd5a5bd8757c02083d558a3decbb88
ScottHahn1 <scotthahn2210@gmail.com>
2024-12-09 19:22:40 +0200

    Feature: Added API route for fetching comment replies

---
+++ app/api/comments/replies/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    const url = new URL(req.url);
+    const params = url.searchParams;
+
+    try {
+      const response = await fetch(`https://youtube.googleapis.com/youtube/v3/comments?${params}&key=${process.env.API_KEY}`);
+        const data = await response.json();
+        return NextResponse.json(data, { status: 200 });
+      } catch (error) {
+        console.error('Internal Server Error:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
