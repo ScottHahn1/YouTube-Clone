d3fcea8056d9926b3ccf1120b6f148e4f114d516
ScottHahn1 <scotthahn2210@gmail.com>
2024-12-11 17:36:44 +0200

    Feature: Added Replies component with infinite scrolling for comment replies

---
+++ app/components/video/replies.tsx
@@ -0,0 +1,79 @@
+import Button from "../button";
+import Image from "next/image";
+import { useFetchInfinite } from "@/app/hooks/useFetch";
+import { useState } from "react";
+
+interface Props {
+    parentCommentId: string;
+    totalReplyCount: number;
+}
+
+interface Replies {
+    items: {
+        id: string;
+        snippet: {
+            authorDisplayName: string;
+            authorProfileImageUrl: string;
+            likeCount: number;
+            publishedAt: string;
+            textDisplay: string;
+        }
+    }[];
+    nextPageToken: string;
+}
+
+const Replies = ({ parentCommentId, totalReplyCount }: Props) => {
+    const [fetchData, setFetchData] = useState(false);
+
+    const repliesQueryParams = new URLSearchParams({
+        'part': 'id, snippet',
+        'parentId': parentCommentId,
+        'maxResults': '30'
+    }).toString();
+
+    const { data: replies, fetchNextPage, hasNextPage } = useFetchInfinite<Replies>(`http://localhost:3000/api/comments/replies?${repliesQueryParams}`, ['replies', parentCommentId], fetchData);
+
+    return (
+        <div>
+            <Button 
+                className={'text-blue-700 cursor-pointer'}
+                handleClick={() => setFetchData(true)}
+            >
+                {
+                    totalReplyCount > 0 && (
+                    <p>{totalReplyCount} replies</p>
+                    )
+                } 
+            </Button>
+
+            {replies?.pages.map(page =>
+                page.items.map(reply => (
+                    <div key={reply.id} className='flex gap-4 mt-4'>
+                        <Image
+                            className='rounded-full w-10 h-10'
+                            src={reply.snippet.authorProfileImageUrl}
+                            width={30}
+                            height={30}
+                            alt={`${reply.snippet.authorDisplayName}'s profile image`}
+                        />
+                        <div className='flex flex-col'>
+                            <p>{reply.snippet.authorDisplayName}</p>
+                            <p>{reply.snippet.textDisplay}</p>
+                        </div>
+                    </div>
+                ))
+            )}
+
+            {hasNextPage && (
+                <Button
+                    className='text-blue-700 cursor-pointer'
+                    handleClick={() => fetchNextPage()}
+                >
+                    <p>Show more replies</p>
+                </Button>
+            )}
+        </div>
+    )
+}
+
+export default Replies;
