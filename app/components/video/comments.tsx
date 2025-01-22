6b4a37a59f216a6b61884139631663827b686f8c
ScottHahn1 <scotthahn2210@gmail.com>
2025-01-22 13:20:06 +0200

    Refactor: Add like count and reply icons to Comments component

--- app/components/video/comments.tsx
+++ app/components/video/comments.tsx
@@ -1,6 +1,8 @@
 import { useFetchInfinite } from "@/app/hooks/useFetch";
 import Replies from "./replies";
 import Image from "next/image";
+import { FaChevronDown, FaThumbsUp } from "react-icons/fa";
+import { formatNumbers } from "@/app/utils/formatter";
 
 interface Comments {
     id: string;
@@ -31,30 +33,31 @@ const Comments = ({ videoId }: { videoId: string }) => {
         'videoId': videoId
     }).toString();
 
-    const { data: comments } = useFetchInfinite<Comments>(`http://localhost:3000/api/comments?${commentsQueryParams}`, ['comments', commentsQueryParams]);
-
+    const { data: comments } = useFetchInfinite<Comments>(`/api/comments?${commentsQueryParams}`, ['comments', commentsQueryParams]);
+    
     return (
         <div>
             {comments?.pages.map(thread => thread.items.map(comment => (
                 <div key={comment.snippet.topLevelComment.id} className='mt-16'>
                     <div className='flex gap-4'>
                         <Image 
-                            className='rounded-full w-10 h-10' 
-                            src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl} 
-                            width={30} height={30} 
+                            className='rounded-full w-10 h-10'
+                            src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl}
+                            width={30} height={30}
                             alt={`${comment.snippet.topLevelComment.snippet.authorDisplayName}'s profile image`} 
                         />
                         
                         <div className='flex flex-col'>
-                            <p>
-                                {comment.snippet.topLevelComment.snippet.authorDisplayName}
-                            </p>
-
-                            <p>
-                                {comment.snippet.topLevelComment.snippet.textDisplay}
-                            </p>
-                            
-                            <Replies parentCommentId={comment.snippet.topLevelComment.id} totalReplyCount={comment.snippet.totalReplyCount} />
+                            <p>{comment.snippet.topLevelComment.snippet.authorDisplayName}</p>
+                            <p>{comment.snippet.topLevelComment.snippet.textDisplay}</p>
+                            <div className='flex gap-3 items-center'>
+                                <FaThumbsUp />
+                                {formatNumbers(comment.snippet.topLevelComment.snippet.likeCount)}
+                            </div>
+                            <div className='flex items-center gap-3 pl-2'>
+                                <FaChevronDown className='text-blue-600' />
+                                <Replies parentCommentId={comment.snippet.topLevelComment.id} totalReplyCount={comment.snippet.totalReplyCount} />
+                            </div>
                         </div>
                     </div>
 
