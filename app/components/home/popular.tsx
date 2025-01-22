0d7569a12857eee6dd6c730806e589aa4b67fa33
ScottHahn1 <scotthahn2210@gmail.com>
2025-01-22 13:12:49 +0200

    Feature: Added Popular component with infinite scroll and channel data. - Display popular videos with infinite scrolling via `useFetchInfinite`. - Fetch and integrate channel details using `useChannels`. - Render video cards with channel info, views, and thumbnails.

--- app/components/home/popular.tsx
+++ app/components/home/popular.tsx
@@ -5,78 +5,101 @@ import useChannels from "../../hooks/useChannels";
 import useInfiniteScroll from "@/app/hooks/useInfiniteScroll";
 
 export interface Videos {
-    items: {
-        id: string;
-        contentDetails: {
-            duration: string;
+  items: {
+    id: string;
+    contentDetails: {
+      duration: string;
+    }
+    snippet: {
+      categoryId: string;
+      channelId: string;
+      channelTitle: string;
+      publishedAt: string;
+      thumbnails: {
+        high: {
+          url: string;
         }
-        snippet: {
-            channelId: string;
-            channelTitle: string;
-            publishedAt: string;
-            thumbnails: {
-                high: {
-                    url: string;
-                }
-            }
-            title: string;
-        }
-        statistics: {
-            viewCount: number;
-        }
-    }[];
-    nextPageToken: string;
+      }
+      title: string;
+    }
+    statistics: {
+      viewCount: number;
+    }
+  }[],
+  nextPageToken: string;
 }
 
 export interface ChannelsData {
     items: {
-        id: string;
-        snippet: {
+        brandingSettings: {
+            channel: {
+              description: string;
+              unsubscribedTrailer: string;
+            },
+            image: {
+              bannerExternalUrl: string;
+            }
+          },
+          contentDetails: {
+            relatedPlaylists: {
+              uploads: string;
+            }
+          },
+          snippet: {
+            customUrl: string;
+            description: string;
             thumbnails: {
-                default: {
-                    url: string;
-                }
+              high: {
+                url: string;
+              }
             }
             title: string;
-        }
-    }[]
+          },
+          statistics: {
+            subscriberCount: number;
+            videoCount: number;
+          }
+        id: string;
+    }[],
     nextPageToken: string;
 }
 
 const Popular = () => {
-    const videosQueryParams = new URLSearchParams({
-        'part': 'snippet, statistics',
-        'chart': 'mostPopular',
-        'maxResults': '30'
-    }).toString();
+  const videosQueryParams = new URLSearchParams({
+    'part': 'snippet, statistics',
+    'chart': 'mostPopular',
+    'maxResults': '30'
+  }).toString();
 
-    const { data: videos, fetchNextPage, hasNextPage, isFetchingNextPage } = useFetchInfinite<Videos>(`http://localhost:3000/api/videos?${videosQueryParams}`, ['popular', videosQueryParams]);
+  const { data: videos, fetchNextPage, hasNextPage, isFetchingNextPage } = useFetchInfinite<Videos>(`/api/videos?${videosQueryParams}`, ['popular']);
 
-    const channels = useChannels(videos?.pages.length ? videos?.pages[videos.pages.length - 1].items.map(page => page.snippet.channelId) : []);
+  const channels = useChannels(videos?.pages.length ? videos?.pages[videos.pages.length - 1].items.map(page => page.snippet.channelId) : []);
 
-    const lastItemRef = useInfiniteScroll(fetchNextPage, hasNextPage, isFetchingNextPage);
-
-    return (
-        <div className='w-80% flex flex-wrap float-right'>
-            {
-                videos?.pages.map(page => page.items.map((video, index) => (
-                    <>
-                        <Card 
-                            key={video.id}
-                            channelImage={channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.thumbnails.default.url as string || ''}
-                            channelTitle= {channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.title as string || ''} 
-                            publishedAt={video.snippet.publishedAt}
-                            title={video.snippet.title} 
-                            thumbnail={video.snippet.thumbnails.high.url} 
-                            videoId={video.id}
-                            views={video.statistics.viewCount}
-                        />
-                        { index === page.items.length - 1 && <div ref={lastItemRef} /> }
-                    </>
-                )))
-            }
-        </div>
-    )
+  const lastItemRef = useInfiniteScroll(fetchNextPage, hasNextPage, isFetchingNextPage);
+  
+  return (
+    <div className='w-80% flex flex-wrap float-right'>
+      {
+        videos?.pages.map(page => page.items.map((video, index) => (
+          <>
+            <Card 
+              key={video.id}
+              channelId={video.snippet.channelId}
+              channelImage={channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.thumbnails.high.url as string || ''}
+              channelTitle= {channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.title as string || ''} 
+              containerWidth='w-1/3'
+              publishedAt={video.snippet.publishedAt}
+              title={video.snippet.title} 
+              thumbnail={video.snippet.thumbnails.high.url} 
+              videoId={video.id}
+              views={video.statistics.viewCount}
+            />
+            { index === page.items.length - 1 && <div ref={lastItemRef} /> }
+          </>
+        )))
+      }
+    </div>
+  )
 }
 
 export default Popular;
