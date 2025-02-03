1eec51ab71189a4816bf6ba4c2ec17373d2f33ba
ScottHahn1 <scotthahn2210@gmail.com>
2025-02-03 15:53:43 +0200

    feat: update component with improved layout and changed from server to client

--- app/components/video/watchVideo.tsx
+++ app/components/video/watchVideo.tsx
@@ -1,9 +1,10 @@
+'use client';
 import { useFetch } from "@/app/hooks/useFetch";
 import { formatDate, formatNumbers } from "@/app/utils/formatter";
 import Image from 'next/image';
 import { Videos } from "../home/popular";
 import useChannels from "@/app/hooks/useChannels";
-import CommentThreads from "./comments";
+import CommentThreads from "./commentThreads";
 import RelatedVideos from "./relatedVideos";
 import VideoPlaylist from "./playlist";
 
@@ -23,58 +24,56 @@ const WatchVideo = ({ playlistId, videoId }: Props) => {
   const channel = useChannels(video ? video.items[0].snippet.channelId : '');
 
   return (
-    <>
-      {
-        video && (
-          <div className='bg-zinc-500 flex'>
-            <div className='flex flex-col w-11/12'>
-              <iframe
-                className='w-full'
-                src={`https://www.youtube.com/embed/${videoId}`}
-                allowFullScreen
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              >
-              </iframe>
-              <div className='flex gap-3'>
-                { 
-                  channel.length > 0 && 
-                  <Image 
-                    className='rounded-full w-10 h-10' 
-                    src={channel[0].snippet.thumbnails.high.url} 
-                    width={30} height={30} 
-                    alt={`${channel[0].snippet.title}'s channel image`} 
-                  />
-                }
-                <div>
-                  <p className='font-extrabold'>
-                    {video.items[0].snippet.title}
-                  </p>
-                  <p>
-                    {video.items[0].snippet.channelTitle}
-                  </p>
-                  <div className='flex gap-1 items-center'>
-                    <span>
-                      {formatNumbers(video.items[0].statistics.viewCount)} views
-                    </span>
-                    <span className='text-4xl leading-none align-baseline mb-1'>
-                      &#x00B7;
-                    </span>
-                    <span>
-                      {formatDate(video.items[0].snippet.publishedAt)}
-                    </span>
-                  </div>
-                </div>
+    <div className='flex'>
+      <div className='flex flex-col w-11/12'>
+          <iframe
+            className='w-full h-96'
+            src={`https://www.youtube.com/embed/${videoId}`}
+            allowFullScreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          >
+          </iframe>
+          <div className='flex gap-3'>
+            { 
+              channel.length > 0 && 
+              <Image 
+                className='rounded-full w-10 h-10' 
+                src={channel[0].snippet.thumbnails.high.url} 
+                width={30} height={30} 
+                alt={`${channel[0].snippet.title}'s channel image`} 
+              />
+            }
+            <div>
+              <p className='font-extrabold'>
+                {video?.items[0].snippet.title}
+              </p>
+              <p>
+                {video?.items[0].snippet.channelTitle}
+              </p>
+              <div className='flex gap-1 items-center'>
+                <span>
+                  {video && formatNumbers(video.items[0].statistics.viewCount)} views
+                </span>
+                <span className='text-4xl leading-none align-baseline mb-1'>
+                  &#x00B7;
+                </span>
+                <span>
+                  {video && formatDate(video.items[0].snippet.publishedAt)}
+                </span>
               </div>
-              <CommentThreads videoId={videoId} />
+            </div>
           </div>
-          <div className='flex flex-col'>
-            <VideoPlaylist />
-            <RelatedVideos videoCategoryId={video.items[0].snippet.categoryId} />
-          </div>
+          <CommentThreads videoId={videoId} />
+      </div>
+
+      { 
+        video &&
+        <div className='flex flex-col'>
+          <VideoPlaylist playlistId={playlistId} videoId={videoId} />
+          <RelatedVideos videoCategoryId={video.items[0].snippet.categoryId} />
         </div>
-        )
       }
-    </>
+    </div>
   )
 }
 
