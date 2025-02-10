dace91c673de5be3652cecba0c4d86e57fdff95d
ScottHahn1 <scotthahn2210@gmail.com>
2025-02-10 15:52:27 +0200

    feat: Add ChannelNavbar component with navigation links

---
+++ app/channel/[id]/navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const ChannelNavbar = ({ id }: { id: string }) => {
+    return (
+        <nav className='ml-52'>
+            <ul className='flex gap-4 text-xl'>
+                <li>
+                    <Link href={`/channel/${id}`}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href={`/channel/${id}/videos`}>
+                        Videos
+                    </Link>
+                </li>
+                <li>
+                    Playlists
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default ChannelNavbar;
