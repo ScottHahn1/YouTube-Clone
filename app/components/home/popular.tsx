7b2ca2d4e63b4de169018a2c9ca8261c4849ddf1
ScottHahn1 <scotthahn2210@gmail.com>
2025-03-20 20:13:39 +0200

    feat: update Popular videos component with content details for api call, and improve responsive grid layout

--- app/components/home/popular.tsx
+++ app/components/home/popular.tsx
@@ -3,6 +3,7 @@ import { useFetchInfinite } from "@/app/hooks/useFetch";
 import Card from "../card";
 import useChannels from "../../hooks/useChannels";
 import useInfiniteScroll from "@/app/hooks/useInfiniteScroll";
+import { Fragment } from 'react';
 
 export interface Videos {
   items: {
@@ -30,43 +31,43 @@ export interface Videos {
 }
 
 export interface ChannelsData {
-    items: {
-        brandingSettings: {
-            channel: {
-              description: string;
-              unsubscribedTrailer: string;
-            },
-            image: {
-              bannerExternalUrl: string;
-            }
-          },
-          contentDetails: {
-            relatedPlaylists: {
-              uploads: string;
-            }
-          },
-          snippet: {
-            customUrl: string;
-            description: string;
-            thumbnails: {
-              high: {
-                url: string;
-              }
-            }
-            title: string;
-          },
-          statistics: {
-            subscriberCount: number;
-            videoCount: number;
-          }
-        id: string;
-    }[],
-    nextPageToken: string;
+  items: {
+    brandingSettings: {
+      channel: {
+        description: string;
+        unsubscribedTrailer: string;
+      },
+      image: {
+        bannerExternalUrl: string;
+      }
+    },
+    contentDetails: {
+      relatedPlaylists: {
+        uploads: string;
+      }
+    },
+    snippet: {
+      customUrl: string;
+      description: string;
+      thumbnails: {
+        high: {
+          url: string;
+        }
+      }
+      title: string;
+    },
+    statistics: {
+      subscriberCount: number;
+      videoCount: number;
+    }
+    id: string;
+  }[],
+  nextPageToken: string;
 }
 
 const Popular = () => {
   const videosQueryParams = new URLSearchParams({
-    'part': 'snippet, statistics',
+    'part': 'contentDetails, snippet, statistics',
     'chart': 'mostPopular',
     'maxResults': '30'
   }).toString();
@@ -78,16 +79,16 @@ const Popular = () => {
   const lastItemRef = useInfiniteScroll(fetchNextPage, hasNextPage, isFetchingNextPage);
 
   return (
-    <div className='w-90% flex flex-wrap  float-right'>
+    <div className='grid gap-3 md:grid-cols-4 md:pr-1 md:ml-32 lg:ml-24 lg:grid-cols-3 xl:ml-36'>
       {
         videos?.pages.map(page => page.items.map((video, index) => (
-          <>
+          <Fragment key={video.id}>
             <Card 
-              key={video.id}
               channelId={video.snippet.channelId}
               channelImage={channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.thumbnails.high.url as string || ''}
               channelTitle= {channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.title as string || ''} 
-              containerWidth='w-1/3'
+              duration={video.contentDetails.duration}
+              index={index}
               publishedAt={video.snippet.publishedAt}
               title={video.snippet.title} 
               thumbnail={video.snippet.thumbnails.high.url} 
@@ -95,7 +96,7 @@ const Popular = () => {
               views={video.statistics.viewCount}
             />
             { index === page.items.length - 1 && <div ref={lastItemRef} /> }
-          </>
+          </Fragment>
         )))
       }
     </div>
