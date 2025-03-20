e9c54a0f368a7f2dfdee27dc13c3047d35e67879
ScottHahn1 <scotthahn2210@gmail.com>
2025-03-20 20:21:30 +0200

    feat: add utility function to format video duration(PT) into HH:MM:SS or MM:SS

--- app/utils/formatter.ts
+++ app/utils/formatter.ts
@@ -1,11 +1,22 @@
 import { format } from 'timeago.js';
 
+export const formatDate = (date: string) => {
+    const formattedDate = format(date);
+    return formattedDate;
+}
+
+export const formatDuration = (duration: string) => {
+    const match = duration.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
+
+    const hours = match![1] ? match![1].padStart(2, '0') : '00';
+    const minutes = match![2] ? match![2].padStart(2, '0') : '00';
+    const seconds = match![3] ? match![3].padStart(2, '0') : '00';
+
+    return hours !== '00' ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`;
+}
+
 export const formatNumbers = (num: number) => {
     const formatter = Intl.NumberFormat('en', { notation: 'compact' });
     const formattedNumber = formatter.format(num);
     return formattedNumber;
-}
-export const formatDate = (date: string) => {
-    const formattedDate = format(date);
-    return formattedDate;
 }
