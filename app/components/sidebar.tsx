68fc38e1d0df8b3093ca890700fc94b4d52ee256
ScottHahn1 <scotthahn2210@gmail.com>
2025-03-20 20:10:11 +0200

    feat: improve Sidebar responsiveness and styling for different screen sizes

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -3,20 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Sidebar = () => {
     return (
-        <div className='w-28 h-full  flex flex-col fixed'>
-            <button className='flex flex-col mb-4 pt-4'>
+        <div className='w-full text-xs flex items-center justify-around bottom-0 fixed md:pt-10 md:w-28 md:h-full md:justify-normal md:flex-col md:text-base md:bottom-auto lg:w-24 xl:w-28 xl:pl-4'>
+            <button className='flex flex-col pb-2 pt-2'>
                 <FontAwesomeIcon icon={faHouse} className=' transition-all duration-300' />
                 <p>Home</p>
             </button>
-            <button className='flex flex-col mb-4'>
+            <button className='flex flex-col pb-2 pt-2'>
                 <FontAwesomeIcon icon={faFilm} className='transition-all duration-300' />
                 <p>Shorts</p>
             </button>
-            <button className='flex flex-col mb-4'>
+            <button className='flex flex-col pb-2 pt-2'>
                 <FontAwesomeIcon icon={faPlayCircle} className=' transition-all duration-300' />
                 <p>Subscriptions</p>
             </button>
-            <button className='flex flex-col mb-4'>
+            <button className='flex flex-col pb-2 pt-2'>
                 <FontAwesomeIcon icon={faUser} className='transition-all duration-300' />
                 <p>You</p>
             </button>
