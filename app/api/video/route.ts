8e2bc3b841930e16df5a3915e46bd55edf7d0e91
ScottHahn1 <scotthahn2210@gmail.com>
2025-03-27 19:41:38 +0200

    feat: add Next.js API route to fetch YouTube video data for a single video

---
+++ app/api/video/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const params = url.searchParams.toString();
+
+  try {
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?${params}&key=${process.env.API_KEY}`);
+    const data = await response.json();
+    return NextResponse.json(data.items[0], { status: 200 });
+  } catch (error) {
+    console.error('Internal Server Error:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
