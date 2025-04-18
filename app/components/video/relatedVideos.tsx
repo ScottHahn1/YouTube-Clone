4e818df01232609fe8f34b8ebec705dbd09ea05a
ScottHahn1 <scotthahn2210@gmail.com>
2025-04-18 20:28:41 +0200

    feat: improve layout and include video duration overlay

--- app/components/video/relatedVideos.tsx
+++ app/components/video/relatedVideos.tsx
@@ -2,7 +2,7 @@ import { useFetchInfinite } from "@/app/hooks/useFetch";
 import { Videos } from "../home/popular";
 import Link from "next/link";
 import Image from "next/image";
-import { formatDate, formatNumbers } from "@/app/utils/formatter";
+import { formatDate, formatDuration, formatNumbers } from "@/app/utils/formatter";
 
 interface RelatedVideos {
     id: string;
@@ -17,7 +17,7 @@ const RelatedVideos = ({ videoCategoryId }: Props) => {
     const videosQueryParams = new URLSearchParams({
         'chart': 'mostPopular',
         'maxResults': '20',
-        'part': 'snippet, statistics',
+        'part': 'contentDetails, snippet, statistics',
         'videoCategoryId': videoCategoryId
     }).toString();
 
@@ -27,24 +27,28 @@ const RelatedVideos = ({ videoCategoryId }: Props) => {
         <div>
             {
                 videos?.pages.map(page => page.items.map((video) => (
-                    <div className='flex' key={video.id}>
-                        <div className='relative w-1/2 h-36'>
+                    <div className='flex items-center gap-1 h-28' key={video.id}>
+                        <div className='relative w-40% h-20 xl:h-24'>
                             <Link href={`/watch/${video.id}`}>
                                 <Image 
-                                    className='p-1 rounded-3xl' 
+                                    className='rounded-lg' 
                                     layout='fill'
                                     objectFit='cover'
                                     src={video.snippet.thumbnails.high.url} 
                                     alt={`${video.snippet.title} video thumbnail`}
                                 />
+                                <p className='absolute bottom-2 right-2 bg-black/60 py-0.5 px-1 rounded-md text-white text-sm'>
+                                    {formatDuration(video.contentDetails.duration)}
+                                </p>
                             </Link>
                         </div>
-                        <div className='w-1/2'>
-                            <p className='text-ellipsis'>{video.snippet.title}</p>
+                        
+                        <div className='py-2 w-80% text-sm h-24 text-gray-700 dark:text-gray-300 lg:py-1 lg:w-55%'>
+                            <h1 className='text-ellipsis text-black dark:text-white font-medium line-clamp-2'>{video.snippet.title}</h1>
                             <p>{video.snippet.channelTitle}</p>
                             <div className='flex gap-1 items-center'>
                                 <span>{formatNumbers(video.statistics.viewCount)} views</span>
-                                <span className='text-4xl leading-none align-baseline mb-1'>&#x00B7;</span>
+                                <span className='text-2xl'>&#x00B7;</span>
                                 <span>{formatDate(video.snippet.publishedAt)}</span>
                             </div>
                         </div>
