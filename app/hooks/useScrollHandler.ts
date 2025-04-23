d7b25b804a6838b7d22490e4472ba70a9f15b2e2
ScottHahn1 <scotthahn2210@gmail.com>
2025-04-23 20:45:17 +0200

    Add custom hook to handle scroll-based button visibility
    - Created useScrollHandler to manage scroll event listeners on playlist containers
    - Calls handleScrollCheck to update visibility of prev/next buttons on scroll
    - Cleans up event listeners on unmount

---
+++ app/hooks/useScrollHandler.ts
@@ -0,0 +1,38 @@
+import { Dispatch, SetStateAction, useEffect } from 'react';
+import { handleScrollCheck } from '../utils/scroll';
+import { PlaylistsItems } from '../channel/[id]/page';
+
+const useScrollHandler = (
+  containers: (HTMLDivElement | null)[],
+  playlistsItems: PlaylistsItems[],
+  setShowPrevButton: Dispatch<SetStateAction<boolean[]>>,
+  setShowNextButton: Dispatch<SetStateAction<boolean[]>>
+) => {
+  useEffect(() => {
+    if (!playlistsItems || !containers.length) return;
+
+    const listeners: (() => void)[] = [];
+
+    playlistsItems.forEach((_, index) => {
+      const container = containers[index];
+      
+      if (!container) return;
+
+      const handleScroll = () => {
+        handleScrollCheck(container, index, setShowPrevButton, setShowNextButton);
+      };
+
+      container.addEventListener('scroll', handleScroll);
+
+      handleScroll(); 
+
+      listeners.push(() => container.removeEventListener('scroll', handleScroll));
+    });
+
+    return () => {
+      listeners.forEach(remove => remove());
+    };
+  }, [containers, playlistsItems, setShowPrevButton, setShowNextButton]);
+};
+
+export default useScrollHandler;
