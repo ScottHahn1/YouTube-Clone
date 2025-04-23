10115f584cf5c6b39193338c3d8c04782a5d9825
ScottHahn1 <scotthahn2210@gmail.com>
2025-04-23 20:34:07 +0200

    refactor: simplify ChannelHome by extracting section UI into ChannelSections component

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,12 +1,8 @@
 'use client';
-import Image from "next/image";
-import { formatDate } from "@/app/utils/formatter";
 import { useFetch } from "@/app/hooks/useFetch";
-import { use, useRef, useState } from "react";
-import { handleScroll } from "@/app/utils/scroll";
-import Link from "next/link";
-import Button from "@/app/components/button";
-
+import { use } from "react";
+import ChannelSections from "./home/sections";
+  
 interface ChannelSections {
   contentDetails: {
     channels: string[];
@@ -15,8 +11,8 @@ interface ChannelSections {
   snippet: {
     type: string;
   }
-}
-  
+};
+      
 interface Playlists {
   items: {
     id: string;
@@ -24,8 +20,8 @@ interface Playlists {
       title: string;
     }
   }[]
-}
-  
+};
+
 export interface PlaylistsItems {
   items: {
     id: string;
@@ -54,119 +50,47 @@ interface Props {
 }
 
 const ChannelHome = ({ params }: Props) => {
-    const { id } = use(params); 
-    const extractedId = id.split('-').slice(1).join('-');
-
-    const sectionQueryParams = new URLSearchParams({
-        'channelId': extractedId as string,
-        'part': 'contentDetails, id, snippet'
-    }).toString()
-
-    const { data: sections } = useFetch<ChannelSections[]>(`/api/channelSections?${sectionQueryParams}`, ['channelSections', sectionQueryParams], !!extractedId);
+  const { id } = use(params); 
+  const extractedId = id.split('-').slice(-1).toString();
     
-    const playlistsIds = sections?.map(section => {
-        if (section.contentDetails.channels) {
-            return section.contentDetails.channels[0]
-        } else if (section.contentDetails.playlists) {
-            return section.contentDetails.playlists[0]
-        }
-    }).join(',')
-    
-    const playlistsQueryParams = new URLSearchParams({
-        'id': playlistsIds as string,
-        'part': 'snippet',
-    }).toString();
+  const sectionQueryParams = new URLSearchParams({
+    'channelId': extractedId as string,
+    'part': 'contentDetails, id, snippet'
+  }).toString();
+  
+  const { data: sections } = useFetch<ChannelSections[]>(`/api/channelSections?${sectionQueryParams}`, ['channelSections', sectionQueryParams], !!extractedId);
 
-    const { data: playlists } = useFetch<Playlists>(`/api/playlists?${playlistsQueryParams}`, ['channelSectionsPlaylists', playlistsQueryParams as string], !!playlistsIds);
+  const playlistsIds = sections
+  ?.map(section => section.contentDetails.playlists?.[0] || section.contentDetails.channels?.[0])
+  .filter(Boolean)
+  .join(',');
+  
+  const playlistsQueryParams = new URLSearchParams({
+    'id': playlistsIds as string,
+    'part': 'snippet',
+  }).toString();
 
-    const { data: playlistsItems } = useFetch<PlaylistsItems[]>(`/api/playlists/items?playlistId=${playlistsIds}`, ['playlistsItems', playlistsIds as string], !!playlistsIds);
- 
-    const [nextButtonPressed, setNextButtonPressed] = useState<boolean[]>(new Array(playlistsItems?.length).fill(false));
+  const { data: playlists } = useFetch<Playlists>(`/api/playlists?${playlistsQueryParams}`, ['channelSectionsPlaylists', playlistsQueryParams as string], !!playlistsIds);
 
-    const scrollContainerRef = useRef<(HTMLDivElement | null)[]>([]);
+  const { data: playlistsItems } = useFetch<PlaylistsItems[]>(`/api/playlists/items?playlistId=${playlistsIds}`, ['playlistsItems', playlistsIds as string], !!playlistsIds);
+      
+  return (
+    <div>
+      {playlists?.items.map((playlist, index) => (
+        <div key={`${playlist.id}-${index}`} className='md:w-full lg:w-[90%] pt-4 mx-1 md:mx-0 '>
+          <p className='text-xl font-semibold md:ml-36'>{playlist.snippet.title}</p>
 
-    return (
-        <div className='w-80% ml-44'>
-            {playlists?.items.map((playlist, index) => (
-                <div key={`${playlist.id}-${index}`} className='pt-4'>
-                    <p className='text-xl font-semibold'>{playlist.snippet.title}</p>
-
-                    <div className='relative pt-4'>
-                        { nextButtonPressed[index] &&
-                            <Button
-                                className='absolute h-10 w-10 left-0 top-1/2 transform -translate-y-1/2 rounded-full bg-gray-700 text-white text-xl leading-none z-10'
-                                handleClick={() => {
-                                    handleScroll({
-                                        container: scrollContainerRef.current[index], 
-                                        direction: 'left', 
-                                        setNextButtonPressed: (value: boolean) => {
-                                            const newState = [...nextButtonPressed];
-                                            newState[index] = value;
-                                            setNextButtonPressed(newState);
-                                        }
-                                    }) 
-                                }}
-                            >
-                                &#8249;
-                            </Button>
-                        }
-                        
-                        <Button
-                            className='absolute h-10 w-10 right-0 top-1/4 rounded-full bg-gray-700 text-white text-3xl font-extralight flex items-center justify-center leading-none z-10'
-                            handleClick={() => {
-                                handleScroll({
-                                    container: scrollContainerRef.current[index], 
-                                    direction: 'right', 
-                                    setNextButtonPressed: (value: boolean) => {
-                                        const newState = [...nextButtonPressed];
-                                        newState[index] = value;
-                                        setNextButtonPressed(newState);
-                                    }
-                                }) 
-                            }}
-                        >
-                            &#8250;
-                        </Button>
-
-                        <div 
-                            className='flex overflow-x-auto scrollbar-hidden' 
-                            ref={(el) => { scrollContainerRef.current[index] = el; }} 
-                        >
-                            {
-                                playlistsItems &&
-                                playlistsItems[index] &&
-                                playlistsItems[index].items &&
-                                playlistsItems[index].items
-                                .slice(0, 15)
-                                .map(item => (
-                                    <div key={item.id} className='w-1/5 flex-shrink-0'>
-                                        <div className='relative w-full h-0 pb-56%'>
-                                            <Link href={`/watch/${item.contentDetails.videoId}`}>
-                                                <Image
-                                                    className='p-1 rounded-3xl object-cover' 
-                                                    fill
-                                                    src={item.snippet.thumbnails.high ? item.snippet.thumbnails.high.url : 'https://i.ytimg.com/vi/default.jpg'}
-                                                    alt={`${item.snippet.title}'s image`}
-                                                />
-                                            </Link>
-                                        </div>
-                                        <Link href={`/watch/${item.contentDetails.videoId}`}>
-                                            <p className='truncate w-48'>{item.snippet.title}</p>
-                                        </Link>
-                                        <div className='border-b border-gray-300 opacity-60 pb-4'>
-                                            <p>{item.snippet.channelTitle}</p>
-                                            <p>{formatDate(item.snippet.publishedAt)}</p>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        
-                    </div>
-                </div>
-            ))} 
+          {
+            playlistsItems &&
+            <ChannelSections 
+              index={index}
+              playlistsItems={playlistsItems}
+            />
+          }
         </div>
-    )
+      ))} 
+    </div>
+  )
 }
 
 export default ChannelHome;
