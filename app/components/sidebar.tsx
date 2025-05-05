8dacbd0f607d8ae26d804a40b1259e0b2b5b5b53
ScottHahn1 <scotthahn2210@gmail.com>
2025-05-05 18:22:35 +0200

    refactor: replace buttons with nav links in sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,30 +1,35 @@
 import { faClockRotateLeft, faFilm, faHouse, faPlayCircle, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const Sidebar = () => {
     return (
-        <div className='w-full text-xs flex items-center gap-2 bg-white dark:bg-charcoal z-10 justify-around bottom-0 fixed md:pt-10 md:w-28 md:h-full md:justify-normal md:flex-col md:text-base md:bottom-auto lg:text-sm lg:w-24 xl:text-base xl:w-28 xl:pl-4'>
-            <button className='flex flex-col pb-2 pt-2'>
-                <FontAwesomeIcon icon={faHouse} className=' transition-all duration-300' />
-                <p>Home</p>
-            </button>
-            <button className='flex flex-col pb-2 pt-2'>
-                <FontAwesomeIcon icon={faFilm} className='transition-all duration-300' />
-                <p>Shorts</p>
-            </button>
-            <button className='flex flex-col pb-2 pt-2'>
-                <FontAwesomeIcon icon={faPlayCircle} className=' transition-all duration-300' />
-                <p>Subscriptions</p>
-            </button>
-            <button className='flex flex-col pb-2 pt-2'>
-                <FontAwesomeIcon icon={faUser} className='transition-all duration-300' />
-                <p>You</p>
-            </button>
-            <button className='flex flex-col'>
-                <FontAwesomeIcon icon={faClockRotateLeft} className='transition-all duration-300' />
-                <p>History</p>
-            </button>
-        </div>
+        <nav className='w-full text-xs flex items-center gap-2 bg-white dark:bg-charcoal z-10 justify-around bottom-0 fixed md:pt-10 md:w-28 md:h-full md:justify-normal md:flex-col md:text-base md:bottom-auto lg:text-sm lg:w-24 xl:text-base xl:w-28 xl:pl-4'>
+            <Link href='/' className='flex flex-col pb-2 pt-2'>
+                <FontAwesomeIcon icon={faHouse} />
+                <span>Home</span>
+            </Link>
+
+            <Link href='/entertainment/24' className='flex flex-col pb-2 pt-2'>
+                <FontAwesomeIcon icon={faFilm} />
+                <span>Entertainment</span>
+            </Link>
+
+            <Link href='/gaming/20' className='flex flex-col pb-2 pt-2'>
+                <FontAwesomeIcon icon={faClockRotateLeft} />
+                <span>Gaming</span>
+            </Link>
+
+            <Link href='/music/10' className='flex flex-col pb-2 pt-2'>
+                <FontAwesomeIcon icon={faPlayCircle} />
+                <span>Music</span>
+            </Link>
+            
+            <Link href='/sports/17' className='flex flex-col pb-2 pt-2'>
+                <FontAwesomeIcon icon={faUser} />
+                <span>Sports</span>
+            </Link>
+        </nav>
     )
 }
 
