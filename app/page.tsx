5fa241ecdf227cca8f37a793fc08914a0307d98b
ScottHahn1 <scotthahn2210@gmail.com>
2025-05-19 19:45:15 +0200

    feat: change component from client to server. pass initial data to child component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,57 @@
 import Popular from "./components/home/popular";
+import fetchData from "./utils/fetchData";
+import Error from "./components/Error";
 
-export default function Home() {
+export interface Videos {
+  items: {
+    id: string;
+    contentDetails: {
+      duration: string;
+    };
+    snippet: {
+      categoryId: string;
+      channelId: string;
+      channelTitle: string;
+      publishedAt: string;
+      thumbnails: {
+        high: {
+          url: string;
+        }
+      }
+      title: string;
+    };
+    statistics: {
+      viewCount: number;
+    };
+  }[];
+  nextPageToken: string;
+};
+
+export default async function Home() {
+  let videos: Videos | null = null;
+
+  const videosQueryParams = new URLSearchParams({
+    'chart': 'mostPopular',
+    'maxResults': '12',
+    'part': 'contentDetails, snippet, statistics'
+  }).toString();
+
+  try {
+    videos = await fetchData<Videos>(
+      `${process.env.API_URL}/api/videos?${videosQueryParams}`, 
+      'Error loading videos!'
+    );
+  } catch (err) {
+    console.error(err);
+    return <Error divClassName='mt-40 md:mt-20 md:ml-36' message='Error loading videos!' />;
+  };
 
   return (
     <main className='mt-4'>
-      <Popular />
+      {
+        videos &&
+        <Popular initialData={videos} />
+      }
     </main>
   );
-}
+};
