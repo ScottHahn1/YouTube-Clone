c399f9e56471d756602c7d6fa85dc03345ee2f87
ScottHahn1 <scotthahn2210@gmail.com>
2025-05-19 20:23:02 +0200

    feat: add reusable function for making infinite queries with initial data

--- app/hooks/useFetch.ts
+++ app/hooks/useFetch.ts
@@ -3,37 +3,79 @@ import { QueryFunctionContext, useInfiniteQuery, useQuery } from "@tanstack/reac
 
 const useFetch = <T>(url: string, queryKey: string[], enabled: boolean) => {
     const fetchData = async () => {
-        const res = await fetch(url)
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Error fetching data: ${res.statusText}`);
+        };
+
         return res.json();
     };
 
     const { data, isLoading, isError, error } = useQuery<T>({
-        queryKey: queryKey,
+        queryKey,
         queryFn: fetchData,
         enabled: enabled,
-        staleTime: 300000 //5 minutes
+        staleTime: 300000 // 5 minutes
     });
 
     return { data, isLoading, isError, error };
-}
+};
 
-const useFetchInfinite = <T extends { nextPageToken: string }>(url: string, queryKey: string[], enabled = true) => {
+const useFetchInfinite = <T extends { nextPageToken: string }>(
+    url: string, queryKey: string[], enabled = true
+) => {
     const fetchData = async ({ pageParam = '' }: QueryFunctionContext) => {
         const fullUrl = pageParam ? `${url}&pageToken=${pageParam}` : url;
         const res = await fetch(fullUrl);
+
+        if (!res.ok) {
+            throw new Error(`Error fetching data: ${res.statusText}`);
+        };
+
         return res.json();
     };
 
-    const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess } = useInfiniteQuery<T>({
-        queryKey: queryKey,
+    const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess } = useInfiniteQuery<T>({
+        queryKey,
         queryFn: fetchData,
         initialPageParam: '',
         getNextPageParam: (lastPage: T) => lastPage.nextPageToken,
         enabled: enabled,
-        staleTime: 300000 //5 minutes
+        staleTime: 300000 // 5 minutes
     });
 
-    return { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess };
-}
+    return { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess };
+};
 
-export { useFetch, useFetchInfinite };
+const useFetchInfiniteWithInitialData = <T extends { nextPageToken: string }>(
+  url: string, queryKey: string[], initialData: T
+) => {
+
+    const fetchData = async ({ pageParam = '' }: QueryFunctionContext) => {
+        const fullUrl = pageParam ? `${url}&pageToken=${pageParam}` : url;
+        const res = await fetch(fullUrl);
+
+        if (!res.ok) {
+            throw new Error(`Error fetching data: ${res.statusText}`);
+        };
+
+        return res.json();
+    };
+
+    const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess } = useInfiniteQuery<T>({
+        queryKey,
+        queryFn: fetchData,
+        initialPageParam: '',
+        getNextPageParam: lastPage => lastPage.nextPageToken,
+        initialData: {
+          pages: [initialData],
+          pageParams: [''],
+        },
+        staleTime: 300000,
+    });
+
+    return { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess };
+};
+
+export { useFetch, useFetchInfinite, useFetchInfiniteWithInitialData };
