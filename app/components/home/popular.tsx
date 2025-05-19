f5163ac95fc2e974d1ffb51bc95122b6e3c82279
ScottHahn1 <scotthahn2210@gmail.com>
2025-05-19 19:49:20 +0200

    feat: preload popular videos with server data

--- app/components/home/popular.tsx
+++ app/components/home/popular.tsx
@@ -1,34 +1,12 @@
 'use client';
-import { useFetchInfinite } from "@/app/hooks/useFetch";
+import { useFetchInfiniteWithInitialData } from "@/app/hooks/useFetch";
 import Card from "../card";
-import useChannels from "../../hooks/useChannels";
+import useChannels from "@/app/hooks/useChannels";
 import useInfiniteScroll from "@/app/hooks/useInfiniteScroll";
 import { Fragment } from 'react';
-
-export interface Videos {
-  items: {
-    id: string;
-    contentDetails: {
-      duration: string;
-    }
-    snippet: {
-      categoryId: string;
-      channelId: string;
-      channelTitle: string;
-      publishedAt: string;
-      thumbnails: {
-        high: {
-          url: string;
-        }
-      }
-      title: string;
-    }
-    statistics: {
-      viewCount: number;
-    }
-  }[],
-  nextPageToken: string;
-}
+import Error from "../Error";
+import Loading from "../Loading";
+import { Videos } from "@/app/page";
 
 export interface ChannelsData {
   items: {
@@ -63,32 +41,43 @@ export interface ChannelsData {
     id: string;
   }[],
   nextPageToken: string;
-}
+};
 
-const Popular = () => {
+interface Props {
+  initialData: Videos;
+};
+
+const Popular = ({ initialData }: Props) => {
   const videosQueryParams = new URLSearchParams({
     'part': 'contentDetails, snippet, statistics',
     'chart': 'mostPopular',
-    'maxResults': '30'
+    'maxResults': '12'
   }).toString();
 
-  const { data: videos, isLoading: isVideosLoading, fetchNextPage, hasNextPage, isFetchingNextPage } = useFetchInfinite<Videos>(`/api/videos?${videosQueryParams}`, ['popular']);
+  const { 
+    data: videos, 
+    isLoading: videosLoading, 
+    isError: videosError, 
+    fetchNextPage, 
+    hasNextPage, 
+    isFetchingNextPage 
+  } = useFetchInfiniteWithInitialData<Videos>(
+    `/api/videos?${videosQueryParams}`, 
+    ['popular'],
+    initialData
+  );
 
-  const { channels, isLoading: isChannelsLoading } = useChannels(videos?.pages.length ? videos?.pages[videos.pages.length - 1].items.map(page => page.snippet.channelId) : []);
+  const [channels, channelsLoading, channelsError] = useChannels(videos?.pages.length ? videos?.pages[videos.pages.length - 1].items.map(page => page.snippet.channelId) : []);
 
   const lastItemRef = useInfiniteScroll(fetchNextPage, hasNextPage, isFetchingNextPage);
 
-  if (isVideosLoading || isChannelsLoading) {
-    return (
-      <div className='flex flex-col bg-blue-700 items-center justify-center bg-gradient-to-br from-white via-gray-100 to-gray-200 animate-pulse'>
-        <div className='w-32 h-32 border-4 mt-20 border-gray-300 border-t-red-600 rounded-full animate-spin mb-4'>
-        </div>
-        <p className='text-xl text-red-700 font-bold'>
-          Loading videos...
-        </p>
-      </div>
-    )
-  }
+  if (videosLoading || channelsLoading) {
+    return <Loading wrapperClassName='min-h-screen md:ml-36' message='Loading videos... '/>;
+  };
+
+  if (videosError || !videos || channelsError || !channels ) {
+    return <Error divClassName='mt-40 md:mt-20 md:ml-36' message='No videos data found!' />;
+  };
 
   return (
     <div className='grid gap-4 md:grid-cols-4 md:pr-1 md:ml-32 lg:grid-cols-3 lg:ml-24 xl:ml-36 xl:pr-0 xl:mr-4'>
@@ -97,23 +86,23 @@ const Popular = () => {
           <Fragment key={video.id}>
             <Card 
               channelId={video.snippet.channelId}
-              channelImage={channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.thumbnails.high.url as string || ''}
-              channelTitle= {channels.length > 0 && channels?.find(channel => channel.id == video.snippet.channelId)?.snippet.title as string || ''} 
+              channelImage={channels?.items.find(channel => channel.id == video.snippet.channelId)?.snippet.thumbnails.high.url as string || ''}
+              channelTitle= {channels?.items.find(channel => channel.id == video.snippet.channelId)?.snippet.title as string || ''} 
               duration={video.contentDetails.duration}
               index={index}
               publishedAt={video.snippet.publishedAt}
               title={video.snippet.title} 
               thumbnail={video.snippet.thumbnails.high.url} 
-              thumbnailSize='w-screen h-44 md:w-auto md:h-40 xl:h-52'
+              thumbnailSize='w-screen h-44 md:w-auto md:h-40 xl:h-56'
               videoId={video.id}
               views={video.statistics.viewCount}
             />
             { index === page.items.length - 1 && <div ref={lastItemRef} /> }
           </Fragment>
         )))
-      }
+      };
     </div>
-  )
-}
+  );
+};
 
 export default Popular;
