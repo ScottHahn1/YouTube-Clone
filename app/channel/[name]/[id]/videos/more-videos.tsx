e3b44c1facf2c7428f0ecbd53530208b9d692ff4
ScottHahn1 <scotthahn2210@gmail.com>
2025-06-02 19:04:18 +0200

    feat: add channel videos page with infinite scroll and error handling

---
+++ app/channel/[name]/[id]/videos/more-videos.tsx
@@ -0,0 +1,120 @@
+'use client';
+import Card from "@/app/components/card";
+import { useFetchInfiniteWithInitialData, useFetchWithState } from "@/app/hooks/useFetch";
+import { Fragment, useEffect } from "react";
+import { ChannelDetails, PlaylistItems } from "./page";
+import useInfiniteScroll from "@/app/hooks/useInfiniteScroll";
+import Error from "@/app/components/Error";
+import Loading from "@/app/components/Loading";
+import { useNavbarVisibility } from "@/app/contexts/channelNavbarContext";
+
+interface MetaData {
+	items: {
+	  contentDetails: {
+	    duration: string;
+	  };
+	  id: string;
+	  statistics: {
+	    likeCount: number;
+	    viewCount: number;
+	  };
+	}[];
+};
+
+interface Props {
+	channelId: string;
+	initialData: PlaylistItems;
+	channelDetails: ChannelDetails;
+	queryKey: string;
+	videosPlaylistId: string;
+	videosQueryParams: string;
+};
+
+const MoreVideos = ({ channelId, initialData, channelDetails, queryKey, videosPlaylistId , videosQueryParams}: Props) => {
+	const { 
+		data: videos, 
+		isLoading: videosLoading, 
+		isError: videosError,
+		fetchNextPage, 
+		hasNextPage, 
+		isFetchingNextPage 
+	} = useFetchInfiniteWithInitialData<PlaylistItems>(
+		`/api/playlists/items?${videosQueryParams}`,
+		[queryKey],
+		initialData
+	);
+
+	const { setShowNavbar } = useNavbarVisibility();
+
+	useEffect(() => {
+    if (initialData) {
+      setShowNavbar(true);
+    };
+  }, [initialData]);
+
+	const videosIds = videos.pages[videos.pages.length - 1].items.map(
+		video => video.snippet.resourceId.videoId
+	);
+
+	const viewsQueryParams = new URLSearchParams({
+		'id': videosIds?.join(','),
+		'maxResults': '12',
+		'part': 'contentDetails, id, snippet, statistics'
+	}).toString();
+
+  	const [metaData] = useFetchWithState<MetaData>(
+		`/api/videos?${viewsQueryParams}`, 
+		['channelVideosMetaData', videos.pages.toString()],
+		!!videosIds
+	);
+
+	const lastItemRef = useInfiniteScroll(fetchNextPage, hasNextPage, isFetchingNextPage);
+
+	if (videosError || !videos || !channelDetails?.items?.length) {
+		return <Error divClassName='mt-40 md:mt-20 md:ml-36' message='No videos data found!' />;
+	};
+
+	if (videosLoading) {
+		return <Loading wrapperClassName='min-h-screen md:ml-36' message='Loading videos... '/>;
+	};
+
+	return (
+		<>
+			<div className='mx-2 pt-4 grid-cols-2 gap-4 md:mx-0 md:grid md:grid-cols-4 md:pr-4 md:ml-32 lg:grid-cols-3 xl:grid-cols-4 xl:ml-36'>
+	      {
+      		metaData && Array.isArray(metaData) &&
+        		videos.pages.map((page, pageIndex) => page.items.map((video, videoIndex) => (
+          	<Fragment key={video.id}>
+            	<Card 
+	              key={video.id}
+	              channelId={channelId}
+	              channelImage={channelDetails.items[0].snippet.thumbnails.high.url}
+	              channelTitle={channelDetails.items[0].snippet.title} 
+          			duration={
+                	metaData[pageIndex] &&
+          				metaData[pageIndex].items.find(item => item.id == video.snippet.resourceId.videoId)
+          				?.contentDetails.duration as string
+          			}
+	              index={videoIndex}
+	              publishedAt={video.snippet.publishedAt}
+	              title={video.snippet.title}
+	              thumbnail={video.snippet.thumbnails.high.url} 
+	              thumbnailSize='h-36'
+	              videoId={video.contentDetails.videoId}
+	              views={
+                	metaData[pageIndex] &&
+          				metaData[pageIndex].items.find(item => item.id == video.snippet.resourceId.videoId)
+          				?.statistics.viewCount as number
+          			}
+	            />
+          	</Fragment>
+        	)))
+	    	}
+
+			<div ref={lastItemRef} />
+	    </div> 
+    </>
+  );
+};
+
+export default MoreVideos;
---
+++ app/channel/[name]/[id]/videos/page.tsx
@@ -0,0 +1,123 @@
+import Error from "@/app/components/Error";
+import MoreVideos from "./more-videos";
+import fetchData from "@/app/utils/fetchData";
+
+export interface ChannelDetails {
+  items: {
+    id: string;
+    brandingSettings: {
+      channel: {
+        description: string;
+        unsubscribedTrailer: string;
+      },
+      image: {
+        bannerExternalUrl: string;
+      }
+    },
+    contentDetails: {
+      relatedPlaylists: {
+        uploads: string;
+      }
+    },
+    snippet: {
+      customUrl: string;
+      description: string;
+      thumbnails: {
+        high: {
+          url: string;
+        }
+      }
+      title: string;
+    },
+    statistics: {
+      subscriberCount: number;
+      videoCount: number;
+    }
+  }[];
+};
+
+export interface PlaylistItems {
+  items: {
+    id: string;
+    contentDetails: {
+      videoId: string;
+    },
+    snippet: {
+      channelTitle: string;
+      publishedAt: string;
+      resourceId: {
+        videoId: string;
+      }
+      thumbnails: {
+        medium: {
+          url: string;
+        }
+        high: {
+          url: string;
+        }
+      }
+      title: string;
+    }
+  }[],
+  nextPageToken: string;
+};
+
+interface Props {
+  params: Promise<{ id: string }>
+};
+
+const ChannelVideos = async ({ params }: Props) => {
+  const { id } = await params;
+
+  let channelDetails: ChannelDetails | null = null;
+  let videos: PlaylistItems | null = null;
+
+  const channelsQueryParams = new URLSearchParams({
+    'id': id,
+    'maxResults': '1',
+    'part': 'brandingSettings, contentDetails, snippet, statistics'
+  }).toString();
+
+  try {
+    channelDetails = await fetchData<ChannelDetails>(
+      `${process.env.API_URL}/api/channels?${channelsQueryParams}`,
+      'Error loading videos!'
+    );
+  } catch(err) {
+    return <Error divClassName='mt-40 md:mt-20 md:ml-36' message="Failed to load channel info!" />;
+  };
+
+  const videosPlaylistId = channelDetails?.items[0]?.contentDetails?.relatedPlaylists?.uploads;
+
+  const videosQueryParams = new URLSearchParams({
+    'maxResults': '12',
+    'part': 'contentDetails, snippet',
+    'playlistId': videosPlaylistId as string
+  }).toString();
+  
+  try {
+    videos = await fetchData<PlaylistItems>(
+      `${process.env.API_URL}/api/playlists/items?${videosQueryParams}`,
+      'No video data found!'
+    );
+  } catch(err) {
+    return <Error divClassName='mt-40 md:mt-20 md:ml-36' message='Failed to load data for videos!' />;
+  };
+
+  if (!videos || !channelDetails || !videosPlaylistId) {
+    return null;
+  };
+
+  return (
+    <MoreVideos 
+      channelId={id} 
+      initialData={videos} 
+      channelDetails={channelDetails} 
+      queryKey='channelUploads'
+      videosPlaylistId={videosPlaylistId}
+      videosQueryParams={videosQueryParams}
+    />
+  );
+};
+
+export default ChannelVideos;
