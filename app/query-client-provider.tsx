5deeec45335213636116716b637c7c7f25776244
ScottHahn1 <scotthahn2210@gmail.com>
2025-06-05 16:53:13 +0200

    refactor: add ChannelNavbarProvider and NavbarSidebarWrapper to QueryClientContextProvider

--- app/query-client-provider.tsx
+++ app/query-client-provider.tsx
@@ -1,14 +1,18 @@
-"use client";
-
+'use client';
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChannelNavbarProvider } from "./contexts/channelNavbarContext";
+import NavbarSidebarWrapper from "./NavbarSidebarWrapper";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient();``
 
 export default function QueryClientContextProvider({ children }: { children: ReactNode }) {
   return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChannelNavbarProvider>
+          <NavbarSidebarWrapper />
+          {children}
+        </ChannelNavbarProvider>
+      </QueryClientProvider>
     )
-}
+};
