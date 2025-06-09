8c27bac24a36572693e5c3e9431a5de392c57091
ScottHahn1 <scotthahn2210@gmail.com>
2025-06-09 21:55:47 +0200

    refactor: migrate WatchVideo component to server component with async data fetching

--- app/components/video/watchVideo.tsx
+++ app/components/video/watchVideo.tsx
@@ -1,155 +1,62 @@
-'use client';
-import { useFetch } from "@/app/hooks/useFetch";
-import { formatDate, formatNumbers } from "@/app/utils/formatter";
-import Image from 'next/image';
-import useChannels from "@/app/hooks/useChannels";
 import CommentThreads from "./commentThreads";
 import RelatedVideos from "./relatedVideos";
 import VideoPlaylist from "./playlist";
 import { Video } from '@/app/search/[query]/type/video';
-import { Fragment, useState } from 'react';
-import Button from '../button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import Error from "../Error";
+import { ChannelDetails } from "@/app/channel/[name]/[id]/videos/page";
+import fetchData from "@/app/utils/fetchData";
+import VideoUi from "./videoUi";
 
 interface Props {
   playlistId: string;
   videoId: string;
-}
+};
 
-const WatchVideo = ({ playlistId, videoId }: Props) => {
+const WatchVideo = async ({ playlistId, videoId }: Props) => {
+  let video: Video | null = null;
+
   const videoQueryParams = new URLSearchParams({
     'id': videoId,
     'part': 'snippet, statistics'
   }).toString();
 
-  const { data: video } = useFetch<Video>(`/api/video?${videoQueryParams}`, ['video', videoQueryParams], !!videoId);
+  try {
+    video = await fetchData<Video>(
+      `${process.env.API_URL}/api/video?${videoQueryParams}`,
+      'No video data found!'
+    );
+  } catch(err) {
+    return <Error divClassName='mt-40 md:mt-20' message='Failed to load data for video!' />;
+  };
 
-  const channel = useChannels(video ? video.snippet.channelId : '');
+  let channel: ChannelDetails | null = null;
 
-  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+  const channelQueryParams = video?.snippet?.channelId ? new URLSearchParams({
+    'id': video.snippet.channelId,
+    'part': 'brandingSettings, contentDetails, snippet, statistics',
+  }).toString() : '';
+
+  try {
+    channel = await fetchData<ChannelDetails>(
+      `${process.env.API_URL}/api/channels?${channelQueryParams}`,
+      'Error loading channel!'
+    );
+  } catch(err) {
+    return <Error divClassName='mt-40 md:mt-20' message='Failed to load channel info!' />;
+  };
+
+  if (!video || !channel) {
+    return null;
+  };
 
   return (
     <>
       <div className='relative flex flex-col mt-8 md:mt-auto'>
-        <div className='w-full md:w-60% md:h-fit lg:w-65%'>
-          <iframe
-            className='h-48 w-full md:h-64 lg:h-96 lg:rounded-2xl'
-            src={`https://www.youtube.com/embed/${videoId}`}
-            allowFullScreen
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          >
-          </iframe>
-
-          <div className='px-2 mt-2'>
-            <h1 className='font-extrabold md:text-lg xl:text-xl'>
-              {video?.snippet.title}
-            </h1>
-
-            {/* channel info and like/subscribe buttons */}
-            <div>
-              <div className='flex items-end flex-wrap mt-2 md:justify-between lg:mt-0 xl:mt-3'>
-                <div className='flex items-center gap-2 flex-1'>
-                  { 
-                    channel.length > 0 && 
-                    <Image 
-                      className='rounded-full w-8 h-8 md:w-10 md:h-10' 
-                      src={channel[0].snippet.thumbnails.high.url} 
-                      width={30} height={30} 
-                      alt={`${channel[0].snippet.title}'s channel image`} 
-                    />
-                  }
-
-                  <div className='xl:flex xl:flex-col'>
-                    <h2 className='font-medium text-sm md:text-base'>
-                      {video?.snippet.channelTitle}
-                    </h2>
-
-                    <div className='flex gap-1'>
-                      <p className='text-sm'>
-                        {channel.length > 0 && 
-                          formatNumbers(channel[0].statistics.subscriberCount)
-                        } 
-                      </p>
-
-                      <p className='text-sm hidden lg:block xl:block'>
-                        subscribers
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div>
-                  <Button
-                    className='rounded-full px-2 bg-charcoal text-white dark:bg-gray-700 md:mr-20 lg:py-1 xl:ml-20 xl:px-4'
-                    handleClick={() => {}}
-                  >
-                    Subscribe
-                  </Button>
-                </div>
-
-                <div className='flex flex-1 basis-full mt-2 rounded-full md:basis-0'>
-                  <Button 
-                    className='px-2 flex items-center gap-3 bg-gray-200 dark:bg-gray-700 rounded-l-full border-black lg:py-1 xl:px-4' 
-                    handleClick={() => {}}
-                  >
-                    <FontAwesomeIcon 
-                      icon={faThumbsUp} 
-                      color='white' 
-                      style={{ stroke: 'black', strokeWidth: '40' }}
-                    />
-                    {video && formatNumbers(video.statistics.likeCount)}
-                  </Button>
-
-                  <div className='cursor-pointer w-[1px] bg-gray-300 dark:bg-gray-500'>
-                  </div>
-
-                  <Button
-                    className='px-2 flex items-center bg-gray-200 dark:bg-gray-700 rounded-r-full border-black lg:py-1 xl:px-4' 
-                    handleClick={() => {}}
-                  >
-                    <FontAwesomeIcon 
-                      icon={faThumbsDown} 
-                      color='white' 
-                      style={{ stroke: 'black', strokeWidth: '40' }}
-                    />
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* video stats and description */}
-          <div className='bg-gray-200 dark:bg-darkGray rounded-lg py-1 px-3 mt-2 mx-2 lg:mx-0'>
-            <div className='flex items-center gap-1'>
-              <span className='text-sm'>
-                {video && formatNumbers(video.statistics.viewCount)} views
-              </span>
-              <span className='text-3xl'>
-                &#x00B7;
-              </span>
-              <span className='text-sm'>
-                {video && formatDate(video.snippet.publishedAt)}
-              </span>
-            </div>
-
-            <p 
-              className={`
-                ${isDescriptionExpanded ? '' : 'line-clamp-2'} 
-                whitespace-pre-wrap text-sm`
-              }
-            >
-              {video?.snippet.description}
-            </p>
-
-            <Button
-              className='font-semibold'
-              handleClick={() => setIsDescriptionExpanded(!isDescriptionExpanded)}
-            >
-              ...more
-            </Button>
-          </div>
-        </div>
+        <VideoUi
+           videoId={videoId}
+           video={video}
+           channel={channel}
+        />
 
         {
           video &&
@@ -162,7 +69,6 @@ const WatchVideo = ({ playlistId, videoId }: Props) => {
             </div>
           </div>
         }
-
       </div>
 
       <div className='px-2 md:w-60% lg:px-0 lg:w-65%'>
@@ -170,7 +76,7 @@ const WatchVideo = ({ playlistId, videoId }: Props) => {
         <CommentThreads videoId={videoId} />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default WatchVideo;
