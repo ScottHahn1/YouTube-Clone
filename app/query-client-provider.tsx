f44311fbd376cecfd93f456cb31a4e9cedcc91e0
ScottHahn1 <scotthahn2210@gmail.com>
2025-06-12 11:32:00 +0200

    fix: remove unused backticks

--- app/query-client-provider.tsx
+++ app/query-client-provider.tsx
@@ -4,15 +4,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChannelNavbarProvider } from "./contexts/channelNavbarContext";
 import NavbarSidebarWrapper from "./NavbarSidebarWrapper";
 
-const queryClient = new QueryClient();``
+const queryClient = new QueryClient();
 
 export default function QueryClientContextProvider({ children }: { children: ReactNode }) {
   return (
-      <QueryClientProvider client={queryClient}>
-        <ChannelNavbarProvider>
-          <NavbarSidebarWrapper />
-          {children}
-        </ChannelNavbarProvider>
-      </QueryClientProvider>
-    )
+    <QueryClientProvider client={queryClient}>
+      <ChannelNavbarProvider>
+        <NavbarSidebarWrapper />
+        {children}
+      </ChannelNavbarProvider>
+    </QueryClientProvider>
+  )
 };
