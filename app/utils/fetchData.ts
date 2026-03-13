4021b580bb14b0aeb21285af62b56543b164bae1
ScottHahn1 <scotthahn2210@gmail.com>
2026-03-13 10:46:49 +0200

    refactor: add optional fetch options parameter to fetchData utility function

--- app/utils/fetchData.ts
+++ app/utils/fetchData.ts
@@ -1,5 +1,5 @@
-const fetchData = async <T>(url: string, errorMsg: string) => {
-	const res = await fetch(url);
+const fetchData = async <T>(url: string, errorMsg: string, options?: RequestInit) => {
+	const res = await fetch(url, options);
 
 	if (!res.ok) {
 		throw new Error(errorMsg);
